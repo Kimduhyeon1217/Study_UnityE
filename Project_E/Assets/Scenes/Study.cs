94ae6788198ea27f762b47e9a116e736752af318
Kimduhyeon1217 <netcop3@gmail.com>
2025-11-27 12:37:28 +0900

    Update Study.cs

--- Project_E/Assets/Scenes/Study.cs
+++ Project_E/Assets/Scenes/Study.cs
@@ -1,11 +1,20 @@
+using System;
 using UnityEngine;
 
 public class Study : MonoBehaviour
 {
     public string say = "";
+    public char text;
+    public int var = 1;
+    public float var02 = 0.4f;
+    public double var03 = 0.5;
+    public bool var04 = true;
+
     void Start()
     {
         Debug.Log(this.say);
+        Debug.Log(this.text);
+
     }
 
     // Update is called once per frame
